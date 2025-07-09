0c523de6546c404e81614a208b7bdb403997bff7
umergulkaleem <umergulkaleem+github@gmail.com>
2025-07-09 23:43:16 +0500

    assignemnt 6 icodeguru python

---
+++ main.py
@@ -0,0 +1,86 @@
+#Umer Gul Kaleem
+
+#task 1
+
+# print("my name is Umer Gul Kaleem")
+
+# ---------------------------------------------------------
+
+#task 2
+#calculator
+
+# input1 = int(input("enter the first no :"))
+# input2 = int(input("enter the second no"))
+# op = input("enter operation + - * /")
+# if op=="+":
+#     result = input1+input2
+# elif op=="-":
+#     result = input1-input2
+# elif op=="*":
+#     result = input1*input2
+# elif op=="/":
+#     result = input1/input2
+# else:
+#     print("invalid")
+# print("the ans is ",result)
+
+# ---------------------------------------------------------
+
+#task3
+#vote checking
+
+# age = int(input("enter your age"))
+# if age>=18:
+#     print("eligible for vote")
+# else:
+#     print("not eligible")
+
+# ---------------------------------------------------------
+
+# task4
+
+#area of circle
+# radius = int(input("enter radius"))
+# result = (22/7)*radius**2
+# print(result)
+
+# ---------------------------------------------------------
+
+# task5
+# conc of name
+# fname = input("enter first name")
+# sname = input("enter second name")
+
+# print(fname+sname)
+ 
+ # ---------------------------------------------------------
+ 
+# task 6
+
+# length
+
+# input1 = input("enter sentence")
+# print(len(input1))
+
+# ---------------------------------------------------------
+
+# task7
+# f string formatting
+
+# name = input("enter name")
+# namecity = input("enter city name")
+# print(f"your name is {name} and city name  is {namecity}")
+
+# ---------------------------------------------------------
+# task8
+# replace
+
+# inputsentence = input("enter sentence")
+# print(inputsentence)
+# replace = input("enter the word yo be repalce :")
+# newword = input("what to replace :")
+# newsentence = inputsentence.replace(replace,newword)
+
+# print(newsentence)
+
+# ---------------------------------------------------------
